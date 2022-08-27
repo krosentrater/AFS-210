185073ef6d9844af5aff49cf0d20d24fcf8031fe
krosentrater <kyle.rosentrater@bryanuniversity.edu>
2022-08-26 21:28:28 -0700

    Completed Assignment 3.1: Linked

--- Week3/doubly-linked.py
+++ Week3/doubly-linked.py
@@ -1,3 +1,6 @@
+from operator import index
+
+
 class Node:
     # A doubly-linked node.
     def __init__(self, data=None):
@@ -29,21 +32,28 @@ class DoublyLinkedList:
 
     def addFirst(self, data) -> None:
         # Add a node at the front of the list
-        node = Node(data) 
-        node.next = self.head
-        self.head.prev = node 
-        self.head = node
-        node.prev = None 
-        self.count += 1 
+        node = Node(data)
+        self.count += 1
+        if (self.head == None):
+            # First node becomes head and tail if nothing in list
+            self.head = node 
+            self.tail = node
+        else:
+            # If there is already something in list, newly made node.next is the current self.head, self.head.rev becomes the newly made node, and we set that new node as the new head.
+            node.next = self.head
+            self.head.prev = node
+            self.head = node
         
 
     def addLast(self, data) -> None:
         # Add a node at the end of the list
-        node = Node(data) 
-        node.next = self.head 
-        self.head.prev = node 
+        node = Node(data)
+        # Current tail.next needs to point towards the new node. New node has to point previous towards the tail. Tail now points to the new node. Increment the counter. 
+        self.tail.next = node
         node.prev = self.tail
+        self.tail = node
         self.count += 1
+
 
 
     def addAtIndex(self, data, index):
@@ -51,26 +61,41 @@ class DoublyLinkedList:
         # If index equals to the length of linked list, the node will be appended to the end of linked list
         # If index is greater than the length, the data will not be inserted.
         # This function does not replace the data at the index, but pushes everything else down.
-        node = Node(data) # Creates new node
-        if (index > len(self.count)):
-            raise Exception("Index out of range.")
-        elif (index == len(self.count)):
-            self.addLast(node)
-        else:
-            for i in range(index):
-                #! Lost
-                node.next = self.head.next
-                node.prev = self.head
-                self.head.next = node
+        new_node = Node(data) #? Create new node
+        if (index > self.count): #? If index is the same as length of list
+            raise Exception("Index out of range.") 
+        elif (index < 0): #? If index is negative
+            raise Exception("Index can't be negative.")
+        elif (index == self.count): #? If index is equal to the length of the list, add the item at the end.
+            self.addLast(data)
+        elif (index == 0): #? If the index is equal to index of 0, add the the beginning of the list. 
+            self.addFirst(data)
+        else: #? If none of the above apply, then add at the current position.
+            # Create variables for keeping track of the positions with pointers
+            curr = self.head
+            prev = self.head
+            for n in range(index): #? Loops through the list positions
+                prev = curr 
+                curr = curr.next
+                
+        new_node.next = curr 
+        new_node.prev = prev 
+        curr.prev = new_node
+        prev.next = new_node
+        self.count += 1
 
 
     def indexOf(self, data):
-        # Search through the list. Return the index position if data is found, otherwise return -1    
+        # Search through the list. Return the index position if data is found, otherwise return -1 
+        position = 0   
         current = self.head
         while current:
-            val = self.head.data
-            self.head = self.head.next
-            yield val
+            val = current.data
+            if (data == val):
+                return position
+            current = current.next
+            position += 1
+        return -1
 
 
     def add(self, data) -> None:
@@ -152,3 +177,19 @@ class DoublyLinkedList:
         return myStr
 
 
+
+words = DoublyLinkedList()
+words.addFirst("May")
+words.add("the")
+words.add("Force")
+words.add("be")
+words.add("with")
+words.add("you")
+words.add("!")
+
+print(words)
+indexOfWith = words.indexOf("with")
+print(indexOfWith)
+words.__setitem__(5, "us")
+words.addAtIndex("all", 6)
+print(words)
