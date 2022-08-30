61fc7087b9164afb167364748a6993c55257e1ee
krosentrater <kyle.rosentrater@bryanuniversity.edu>
2022-08-30 10:45:23 -0700

    Expected palindrome assignment result

---
+++ Week3/LIFO.py
@@ -0,0 +1,38 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+    
+    def push(self, data):
+        self.items.append(data)
+
+    def pop(self):
+        return self.items.pop()
+    
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+    
+    def enqueue(self, data):
+        self.items.insert(0, data)
+
+    def dequeue(self):
+        return self.items(-1)
+    
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
